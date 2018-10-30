913417ee55a6e5e73f87ea74106c1a81bbe13536
BryanDesrosiers1999 <39912053+BryanDesrosiers1999@users.noreply.github.com>
2018-10-30 14:29:38 -0400

    jkjk

--- app/src/main/java/com/platelist/platelist_real/home_Screen.java
+++ app/src/main/java/com/platelist/platelist_real/home_Screen.java
@@ -7,12 +7,16 @@ import android.view.View;
 
 public class home_Screen extends AppCompatActivity {
 
-    @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home__screen);
     }
     public void maps_click(View view) {
+        Intent intent = new Intent(this,platelist_Maps.class);
+        startActivity(intent);
+    }
+    public void search_click(View view) {
         Intent intent = new Intent(this,platelist_Maps.class);
         startActivity(intent);
     }
